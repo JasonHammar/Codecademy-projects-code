fe187bbe123f821ae9d36c046b590b68dfc32566
JasonHammar <66536567+JasonHammar@users.noreply.github.com>
2021-10-04 16:36:57 -0700

    Create app.js

---
+++ JavaScript Projects/Dog Years/app.js
@@ -0,0 +1,9 @@
+let myAge = 22;// My current age as a variable
+let earlyYears = 2;// This variable represents my early years
+earlyYears *= 10.5;
+let laterYears = myAge - 2;// represents my later years.
+laterYears *= 4;// Multiplies lateryears by 4.
+// Below variable calculates your age in dog years.
+let myAgeInDogYears = earlyYears + laterYears;
+var myName = 'Jason Hammar'.toLowerCase();// Stores my name.
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
