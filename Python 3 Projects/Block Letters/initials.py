d0acef31224a716a8a3ac780b1b5d9a12459ef2e
JasonHammar <66536567+JasonHammar@users.noreply.github.com>
2021-10-05 16:11:28 -0700

    Create initials.py

---
+++ Python 3 Projects/Block Letters/initials.py
@@ -0,0 +1,10 @@
+# Jason Hammar
+
+# The below code will print my initials in block letters
+print("JJJJJ H   H")
+print("  J   H   H")
+print("  J   H   H")
+print("  J   HHHHH")
+print("J J   H   H")
+print("J J   H   H")
+print(" JJ   H   H")
