2a4797ff4807e2ecdffa027ff040fbf42a7942f7
JasonHammar <66536567+JasonHammar@users.noreply.github.com>
2022-05-26 16:43:39 -0700

    Create MathMagic.kt

---
+++ Kotlin Projects/Math Magic/MathMagic.kt
@@ -0,0 +1,11 @@
+fun main() {
+  // This program is calculating my age through various operations.
+  val userName = "Jason Hammar"
+  var magicNum: Int
+  magicNum = 2 * 5// Multiplies the first digit of my age by 5
+  magicNum += 3// Adds 3 to magicNum
+  magicNum *= 2// Multiplies 2 to magicNum
+  magicNum += 2// Adds the second digit of my age to magicNum
+  magicNum -= 6// Subtracts 6 from magicNum
+  println(userName + "\'s age is " + magicNum)// Prints out the age.
+}
