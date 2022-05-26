356c082ee6bcc63859aa83b68d4555f805d44ca6
JasonHammar <66536567+JasonHammar@users.noreply.github.com>
2022-05-26 15:49:40 -0700

    Create Art.kt

---
+++ Kotlin Projects/Ascii Art/Art.kt
@@ -0,0 +1,25 @@
+// Write your code below
+
+/*
+      .....      
+  .         .   
+ .   0  0    .  
+  ...........   
+   .. __ ..     
+  . .|C_|. .    
+ .  ......  .   
+     .  .       
+     .  .
+*/
+
+fun main(){// The main function will create an ascii drawing of a robot in the output.
+  println("     .....")
+  println("  .         .")
+  println(" .   0  0    .")
+  println("  ...........")
+  println("   .. __ ..")
+  println("  . .|C_|. .")
+  println(" .  ......  .")
+  println("     .  .")
+  println("     .  .")
+}
