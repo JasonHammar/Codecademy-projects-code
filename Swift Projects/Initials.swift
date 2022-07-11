ae1f928840f50c4ebee29aedae4f6cfd9381fa77
JasonHammar <66536567+JasonHammar@users.noreply.github.com>
2022-07-11 12:35:46 -0700

    Create Initials.swift

---
+++ Swift Projects/Initials.swift
@@ -0,0 +1,11 @@
+// I once ran a mile at 5 minutes
+
+
+// This will print out my initials in the terminal.
+print("JJJJJ  H   H")
+print("  J    H   H")
+print("  J    H   H")
+print("  J    HHHHH")
+print("J J    H   H")
+print("J J    H   H")
+print(" JJ    H   H")
