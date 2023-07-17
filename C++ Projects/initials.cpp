e5cd2f42fb0cf272b0ed067bb28bce77efd9ee0d
JasonHammar <66536567+JasonHammar@users.noreply.github.com>
2023-07-17 13:44:20 -0700

    Create initials.cpp

---
+++ C++ Projects/initials.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// This main method prints out my initials.
+int main(){
+  std::cout << "    JJJJJ H   H\n";
+  std::cout << "      J   H   H\n";
+  std::cout << "      J   H   H\n";
+  std::cout << "      J   HHHHH\n";
+  std::cout << "J     J   H   H\n";
+  std::cout << "J     J   H   H\n";
+  std::cout << " J  J     H   H\n";
+}
